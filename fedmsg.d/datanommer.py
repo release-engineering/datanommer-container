c488a41220051b9c2f69685ac7eb28a73e0c42e5
Ralph Bean <rbean@redhat.com>
2017-06-26 17:03:54 -0400

    Typofix.

--- fedmsg.d/datanommer.py
+++ fedmsg.d/datanommer.py
@@ -17,7 +17,7 @@ config = {
 
     'datagrepper_logo': 'static/datagrepper-debranded.png',
     'theme_css_url': 'static/pnt-bootstrap.min.css',
-    'message_bus_link': 'https://mojo.redhat.com/docs/DOC-1072237',
+    'message_bus_docs': 'https://mojo.redhat.com/docs/DOC-1072237',
     'message_bus_shortname': 'UMB',
     'message_bus_longname': 'Unified Message Bus (UMB)',
 }
