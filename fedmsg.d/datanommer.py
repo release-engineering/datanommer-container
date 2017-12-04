6e74f95dd844e9b141075e1d2a610c57a9384591
Mike Bonnet <mikeb@redhat.com>
2017-12-04 14:21:30 -0500

    support reading from multiple topics

--- fedmsg.d/datanommer.py
+++ fedmsg.d/datanommer.py
@@ -4,6 +4,7 @@ DB_HOST     = os.environ['DB_HOST']
 DB_NAME     = os.environ['DB_NAME']
 DB_USERNAME = os.environ['DB_USERNAME']
 DB_PASSWORD = os.environ['DB_PASSWORD']
+TOPICS      = os.environ['DATANOMMER_TOPICS'].split()
 
 config = {
     'datanommer.enabled': True,
@@ -11,5 +12,5 @@ config = {
                                                                        DB_PASSWORD,
                                                                        DB_HOST,
                                                                        DB_NAME),
-    'datanommer.topic': os.environ['DATANOMMER_TOPIC'],
+    'datanommer.topic': TOPICS,
 }
